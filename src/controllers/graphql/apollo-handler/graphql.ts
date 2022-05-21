80521abfd20d937109495d886820a0f658743d35
Rares <jianu.rares30@gmail.com>
2022-05-21 20:39:31 +0300

    chore: try to serve gphql offline

--- src/controllers/graphql/apollo-handler/graphql.ts
+++ src/controllers/graphql/apollo-handler/graphql.ts
@@ -1,1 +1,25 @@
 import { ApolloServer, gql } from 'apollo-server-lambda';
+import User from '../users/type-defs';
+import { resolver as userResolver } from '../users/users.schema';
+import { GQLContext } from 'types';
+import { buildSubgraphSchema } from '@apollo/federation';
+import merge from 'lodash.merge';
+
+const schema = buildSubgraphSchema({
+  typeDefs: [User],
+  resolvers: merge(userResolver),
+});
+
+const services = {};
+
+const server = new ApolloServer({
+  schema,
+  context: async ({ context, event }) =>
+    ({
+      reqHeaders: event.headers,
+      services,
+      cache: {},
+    } as GQLContext),
+});
+
+export const handler = server.createHandler();
--- src/controllers/graphql/users/users.schema.ts
+++ src/controllers/graphql/users/users.schema.ts
@@ -1,7 +1,7 @@
 export const resolver = {
   Query: {
-    user(spending, args, context) {
-      return context.services.users.get();
+    user() {
+      return 'Hello';
     },
   },
 };
